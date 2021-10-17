cf185f788752ab700916366449729ff887cdd02f
LAKSHMI R <82307120+R-Lakshmi-shinde@users.noreply.github.com>
2021-10-17 18:10:39 +0530

    Update StringCalculator.java
    Empty string return zero

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,18 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+      if(isEmpty(input))
+      {
+          return 0;
+      }
+        return stringToInt(input);
     }
-
+    public booleaan isEmpty(string input)
+    {
+        return input.isEmpty();
+    }
+    private int StringToInt(String input)
+    {
+        return Integer.parseInt(input);
+    }
 }
